8735ccacf0223768f569b08354c768584323a3ee
klaz3r <klazer220033@gmail.com>
2023-10-30 21:42:48 +0200

    FIrst Draft

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,7 +11,7 @@ export default function Layout({ children }: LayoutProps) {
     <div className="bg-logotipPurple min-h-screen">
       <Navbar />
       {children}
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
